10eaac47cc90c187ad617cd143d3abde4447efb9
Adam Waxman <awaxman11@gmail.com>
2023-02-05 13:01:31 -0500

    Exercise 1 solution video updates

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -8,7 +8,7 @@ const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
 function ToastPlayground() {
   const [message, setMessage] = React.useState('');
-  const [variant, setVariant] = React.useState('');
+  const [variant, setVariant] = React.useState(VARIANT_OPTIONS[0]);
 
   return (
     <div className={styles.wrapper}>
@@ -30,6 +30,7 @@ function ToastPlayground() {
             <textarea
               id="message" 
               className={styles.messageInput}
+              value={message}
               onChange={event => {
                 setMessage(event.target.value);
               }}  
@@ -57,22 +58,6 @@ function ToastPlayground() {
                 {option}
               </label>
             ))}
-            {/* <label htmlFor="variant-notice">
-              <input
-                id="variant-notice"
-                type="radio"
-                name="variant"
-                value="notice"
-                onChange={ event => {
-                  setVariant(event.target.value);
-                }
-
-                }
-              />
-              notice
-            </label> */}
-
-            {/* TODO Other Variant radio buttons here */}
           </div>
         </div>
 
